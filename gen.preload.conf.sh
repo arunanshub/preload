2376e991cd2e03da93a7fb0bb36fd87a047bd4de
Arunanshu Biswas <mydellpc07@gmail.com>
2021-10-20 12:45:15 +0530

    build(meson): generate `preload.conf` via buildfile

--- gen.preload.conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IN="$1"
-KEYS="$2"
-
-I='[ \t]*\([^, \t]*\)[ \t]*'
-SEDSCRIPT=`sed -e "s/confkey($I,$I,$I,$I,$I)/s@default_\3@\4@g; s@unit_\3@\5@g;/;" < "$KEYS"`
-
-sed -e "$SEDSCRIPT" "$IN"
