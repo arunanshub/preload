b4851891d0109f3c8b545cd62692a645cc1eb7ab
Arunanshu Biswas <mydellpc07@gmail.com>
2021-11-18 20:34:18 +0530

    test: run tests with meson

--- runtests.sh
+++ runtests.sh
@@ -2,13 +2,28 @@
 
 set -e
 
-> preload.state
+test_debug() {
+    ( sleep 1; killall $APPNAME 2>/dev/null ) &
+    $APPNAME -c preload.conf -s '' -d
+}
 
-echo Testing preload with preconf.conf.debug config file
-( sleep 1; killall ./preload 2>/dev/null ) &
-./preload -c preload.conf -s '' -d
+test_normal() {
+    ( sleep 1; killall $APPNAME 2>/dev/null ) &
+    $APPNAME -c preload.conf -s '' -l '' -f
+}
 
-echo Testing preload with preconf.conf config file
-( sleep 1; killall ./preload 2>/dev/null ) &
-./preload -c preload.conf -s '' -l '' -f
+main() {
+    case $1 in
+        debug)
+            test_debug
+            ;;
+        normal)
+            test_normal
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
 
+main $1
